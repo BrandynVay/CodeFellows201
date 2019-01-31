e108e2d84da7828587d64a44b22fa76cc8ed5f09
KlNGU <brandynvay@yahoo.com>
2019-01-30 20:04:48 -0800

    Finally the javascript is working

--- Projects/About Me (Guessing Game)/AboutMe.js
+++ Projects/About Me (Guessing Game)/AboutMe.js
@@ -1,3 +1,5 @@
+'use strict'
+
 var name = prompt ('Who do you think I am?');
 console.log (name);
 
@@ -5,32 +7,40 @@ if (name !== 'Brandyn'){
     name = prompt ('Who do you think I am?');
 }
 
-var Dogs = prompt ( name + ' , very good. Yes my name is Brandyn, can you guess what my favorite animal is? Ill give you hint, it has a tale.');
-console.log (Dogs);
+var dogs = prompt (name 
+    + ' , very good. Yes my name is Brandyn, can you guess what my favorite animal is? I\'ll give you hint, it has a tale.');
+console.log (dogs);
 
-if (Dogs !== 'Dogs'){
-    Dogs = prompt ('Can you guess my favorite animal?');
+if (dogs !== 'Dogs'){
+    dogs = prompt ('Can you guess my favorite animal?');
 }
 
-var drawing = prompt (dogs + ' ,very good. Now can you try to guess what my favorite hobby is? Ill give you a hint, It has to do with a pen and paper.');
+var drawing = prompt (dogs 
+    + ' ,very good. Now can you try to guess what my favorite hobby is? I\'ll give you a hint, It has to do with a pen and paper.');
 console.log (drawing);
 
 if (drawing !== 'Drawing'){
     drawing = prompt ('Can you guess my favorite hobby?');
 }
 
-var snowboarding = prompt (drawing + ' ,your doing great. I wonder if you might know my favorite sport to do outside? Ill give you a hint, it has to do with the snow.');
+var snowboarding = prompt (drawing 
+    + ' ,your doing great. I wonder if you might know my favorite sport to do outside? I\'ll give you a hint, it has to do with the snow.');
 console.log (snowboarding);
 
-if (snowboarding !== 'snowboarding'){
+if (snowboarding !== 'Snowboarding'){
     snowboarding = prompt ('What is my favorite sport to do outside?');
 }
 
-var end = prompt (snowboarding + ' ,man you must know me so good already. If you want this to be the end of the random questions, then answer Y or N.');
+var end = prompt (snowboarding 
+    + ' ,man you must know me so well already. If you want this to be the end of the random questions, then answer Y or N.');
 console.log (end);
 
 if (end === 'Y'){
-    alert('GOOD, so am I. But my name is ' + name + ' and i love ' + dogs + ' so much that I dont think i can live without one. Then one of my other favorite things to do is ' + drawing + ' . It alows me to have a creative outlit, that i just enjoy. Then last but not least is what i love to do when i have a chance, that would be ' + snowboarding + ' . Welp thats everything you need to know about me, maybe. We can leave that for another time, I hope you have a great day......Bye.');
+    alert('GOOD, so am I. But my name is ' + name 
+    + ' and i love ' + dogs 
+    + ' so much that I dont think I can live without one. Then one of my other favorite things to do is ' + drawing 
+    + ' . It alows me to have a creative outlit, that I just enjoy. Then last but not least is what I love to do when I have a chance, that would be ' + snowboarding 
+    + ' . Welp thats everything you need to know about me, maybe. We can leave that for another time, I hope you have a great day......Bye.');
 }else{
     alert('Well your no fun.....BYE!!!!');
-} 
+}
